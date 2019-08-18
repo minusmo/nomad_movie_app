09628c1b6f93c9bd76b9a24c51afeff5de7708e2
hojooneum <bldolphin96@gmail.com>
2019-08-18 17:05:57 +0900

    nomad react course on #2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ fav }) {
+  return <h3>I love {fav}</h3>;
+}
+
+const FoodILike = [
+  { name: "kimchi" },
+  { name: "cake" },
+  { name: "bread" },
+  { name: "bun" }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {FoodILike.map(dish => (
+        <Food fav={dish.name} />
+      ))}
     </div>
   );
 }
