7355dab72c24881b5520198ba8cda4d6bd5da876
hojooneum <bldolphin96@gmail.com>
2019-08-26 21:54:00 +0900

    rating props added, propTypes added , id as keyadd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ fav }) {
-  return <h3>I love {fav}</h3>;
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h3>I love {name}</h3>
+      <h4>{rating}/5.0</h4>
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+function renderFood(dish) {
+  console.log(dish);
+  return <Food key={dish.id} name={dish.name} rating={dish.rating} />;
 }
 
 const FoodILike = [
-  { name: "kimchi" },
-  { name: "cake" },
-  { name: "bread" },
-  { name: "bun" }
+  { id: 1, name: "kimchi", rating: 5 },
+  { id: 2, name: "cake", rating: 4 },
+  { id: 3, name: "bread", rating: 3 },
+  { id: 4, name: "bun", rating: 2 }
 ];
 
 function App() {
-  return (
-    <div>
-      {FoodILike.map(dish => (
-        <Food fav={dish.name} />
-      ))}
-    </div>
-  );
+  return <div>{FoodILike.map(renderFood)}</div>;
 }
 
 export default App;
