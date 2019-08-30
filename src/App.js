c2fb337bfe7e8e794e5d0b35223bf23d052b0064
hojooneum <bldolphin96@gmail.com>
2019-08-30 23:37:59 +0900

    fetching movie.json data from yts using axios, used async, await.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
+import { async } from "q";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false }, 6000);
-    });
+    this.getMovies();
   }
 
   render() {
